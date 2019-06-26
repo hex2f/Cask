b4e1488afd81512d6128281ff0e8bcbd36ceac75
rekkyrek <hampus0505@gmail.com>
2019-06-27 00:04:45 +0200

    Added DB

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 const Bot = require('./lib')
 const config = require('./config.json')
 
+const mongoose = require('mongoose')
+const { Script } = require('./models')
+
+mongoose.connect(`mongodb://${config.db.addr}`, { auth: { authSource: 'admin' }, user: config.db.user, pass: config.db.pass, useNewUrlParser: true })
+
 const bot = new Bot(config)
+
+bot.on('command', async ({ key, msg }) => {
+  let script = await Script.findOne({
+    guild: msg.guild.id,
+    key: key
+  })
+
+  console.log(script)
+})
