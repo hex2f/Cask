7eb46387cf1571c74957b81f1dbb667cf9cae7f8
rekkyrek <hampus0505@gmail.com>
2019-06-27 00:07:52 +0200

    forgot node doesnt have export const lol

--- models.js
+++ models.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-export const Script = mongoose.model('scripts', {
+module.exports.Script = mongoose.model('scripts', {
   guild: { type: String, required: true },
   key: { type: String, required: true },
   code: { type: String, required: true }
