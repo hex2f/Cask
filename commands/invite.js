df669073a79ac6a3543d3ea18867a8f907235aa6
rekkyrek <hampus0505@gmail.com>
2019-06-28 23:26:41 +0200

    Changed permissions for invite

--- commands/invite.js
+++ commands/invite.js
@@ -6,9 +6,9 @@ module.exports = class {
 
   async trigger (msg) {
     try {
-      msg.author.send(`https://discordapp.com/oauth2/authorize?client_id=593852428626165760&scope=bot&permissions=8`)
+      msg.author.send(`https://discordapp.com/oauth2/authorize?client_id=593852428626165760&scope=bot&permissions=2101869815`)
     } catch (e) {
-      msg.reply(`https://discordapp.com/oauth2/authorize?client_id=593852428626165760&scope=bot&permissions=8`)
+      msg.reply(`https://discordapp.com/oauth2/authorize?client_id=593852428626165760&scope=bot&permissions=2101869815`)
     }
 
     msg.delete()
